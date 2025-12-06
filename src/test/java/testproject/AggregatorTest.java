1d6715470f44b3ec176ad31d14bbacb9a36f72c5
Nikita <nikita.kalugin.03@inbox.ru>
2025-12-06 23:12:10 +0300

    add test

--- src/test/java/testproject/AggregatorTest.java
+++ src/test/java/testproject/AggregatorTest.java
@@ -27,4 +27,47 @@ public class AggregatorTest {
         agr.addValue(val);
         assertThat(agr.getSum()).isEqualTo(val * 2);
     }
+
+    @Test
+    public void testReset() {
+        Aggregator agr = new Aggregator();
+        agr.addValue(10.5);
+        agr.addValue(20.3);
+        assertThat(agr.getSum()).isEqualTo(30.8);
+        agr.reset();
+        assertThat(agr.getSum()).isZero();
+    }
+
+    @Test
+    public void testResetAndContinue() {
+        Aggregator agr = new Aggregator();
+        agr.addValue(10);
+        agr.reset();
+        agr.addValue(5);
+        assertThat(agr.getSum()).isEqualTo(5);
+    }
+
+    @Test
+    public void testAddNegativeValues() {
+        Aggregator agr = new Aggregator();
+        agr.addValue(10);
+        agr.addValue(-5);
+        assertThat(agr.getSum()).isEqualTo(5);
+    }
+
+    @Test
+    public void testAddZero() {
+        Aggregator agr = new Aggregator();
+        agr.addValue(10);
+        agr.addValue(0);
+        assertThat(agr.getSum()).isEqualTo(10);
+    }
+
+    @Test
+    public void testAddDecimalValues() {
+        Aggregator agr = new Aggregator();
+        agr.addValue(1.5);
+        agr.addValue(2.7);
+        assertThat(agr.getSum()).isEqualTo(4.2);
+    }
 }
--- src/test/java/testproject/CalculatorTest.java
+++ src/test/java/testproject/CalculatorTest.java
@@ -14,4 +14,32 @@ public class CalculatorTest {
     public void testSubtract() {
         assertThat(Calculator.subtract(3,2)).isEqualTo(1);
     }
+
+    @Test
+    public void testSumWithZero() {
+        assertThat(Calculator.sum(5, 0)).isEqualTo(5);
+        assertThat(Calculator.sum(0, 5)).isEqualTo(5);
+        assertThat(Calculator.sum(0, 0)).isEqualTo(0);
+    }
+
+    @Test
+    public void testSumWithNegativeNumbers() {
+        assertThat(Calculator.sum(-5, -3)).isEqualTo(-8);
+        assertThat(Calculator.sum(-5, 3)).isEqualTo(-2);
+        assertThat(Calculator.sum(5, -3)).isEqualTo(2);
+    }
+
+    @Test
+    public void testSubtractWithZero() {
+        assertThat(Calculator.subtract(5, 0)).isEqualTo(5);
+        assertThat(Calculator.subtract(0, 5)).isEqualTo(-5);
+        assertThat(Calculator.subtract(0, 0)).isEqualTo(0);
+    }
+
+    @Test
+    public void testSubtractWithNegativeNumbers() {
+        assertThat(Calculator.subtract(-5, -3)).isEqualTo(-2);
+        assertThat(Calculator.subtract(-5, 3)).isEqualTo(-8);
+        assertThat(Calculator.subtract(5, -3)).isEqualTo(8);
+    }
 }
